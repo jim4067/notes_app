53a6ae009d8993d18fcbea64449ce76f45615d8d
jim4067 <jimmyimpulse2@gmail.com>
2020-07-18 16:03:07 +0300

    added an api test. Checking whether the response returnes is OK and in JSON format

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
     testEnvironment : 'node',
+    testTimeout : 300000
 };
--- utils/config.js
+++ utils/config.js
@@ -3,8 +3,12 @@ require('dotenv').config();
 let PORT =  process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
 
+if(process.env.NODE_ENV === 'test'){
+    MONGODB_URI = process.env.TEST_MONGODB_URI
+}
+
 module.exports = {
     PORT, MONGODB_URI
 }
 
-//the handling of enviromntal variables is in this module
+//this module defines the application's configuration
