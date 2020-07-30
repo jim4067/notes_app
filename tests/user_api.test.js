04af8e43594d195e2ad5e0f78f144736ac90fb64
jim4067 <jimmyimpulse2@gmail.com>
2020-07-30 16:54:26 +0300

    all that remains now is to test this test, wait for it to fail, comment it out and continue coding :joy: :joy: joy:

---
+++ tests/user_api.test.js
@@ -0,0 +1,48 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const helper = require('./test_helper');
+const User = require('../models/user');
+
+const api = supertest(app);
+
+describe("when initially there is one user in the DB", () => {
+    beforeEach(async () => {
+        await User.deleteMany({});
+
+        const password_hash = await bcrypt.hash('secret', 10);
+        const user = new User({
+            username: "root",
+            password_hash
+        });
+
+        await user.save();
+    });
+
+    test("creation succeds with a fresh username", async () => {
+        const users_at_start = await helper.users_in_db();
+
+        const new_user = new User({
+            username : "jim_4067",
+            name : "James Mutuku",
+            password : "pass123"
+        });
+
+        await api
+                 .post('/api/users')
+                 .send(new_user)
+                 .expect(200)
+                 .expect('Content-Type', /application\/json/);
+
+        const users_at_end = await helper.users_in_db();
+        expect(users_at_end).toHaveLength(users_at_start.length + 1);
+
+        const usernames = users_at_end.map(u_param => u_param.username);
+        expect(usernames).toContain(new_user.username);
+    });
+});
+
+afterAll(() => {
+    mongoose.connection.close();
+});
