06269b98c6dabbc8d8677f2544d208bbc02fa18f
jim4067 <jimmyimpulse2@gmail.com>
2020-07-31 14:50:01 +0300

    :rocket:

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,7 @@ user_router.get('/', async (req, res) => {
     const users = await User
         .find({})
         .populate('notes', { content: 1, date: 1 });
-        
+
     res.json(users);
 });
 
