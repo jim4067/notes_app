4fe582fbb43b795579a54e0aa84036b80828ef74
jim4067 <jimmyimpulse2@gmail.com>
2020-08-08 00:47:03 +0300

    :rocket: :rocket: :rocket: :rocket:

--- controllers/login.js
+++ controllers/login.js
@@ -2,3 +2,18 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const login_router = require('express').Router;
 const User = require('../models/user');
+
+login_router.post('/', async (req, res) => {
+    const body = req.body;
+
+    const user = await User.findOne({ username: body.username });
+    const password_correct = user === null
+        ? false
+        : await bcrypt.compare(body.password, user.password_hash);
+
+    if (!(user && password_correct)){
+        return res.status(401).json({
+            error : "invalid username or password"
+        });
+    }
+});
