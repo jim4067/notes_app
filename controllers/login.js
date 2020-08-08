dd9f03c024e154e977c5bd5f3e261ad8bae8c4f9
jim4067 <jimmyimpulse2@gmail.com>
2020-08-08 19:18:50 +0300

    :rocket: code :rocket: formatted :rocket:

--- controllers/login.js
+++ controllers/login.js
@@ -9,30 +9,32 @@ login_router.post('/', async (req, res) => {
     const user = await User.findOne({ username: body.username });
 
     //user not found
-    if(!user){
-        return res.status(401).json({error : "user does not exist" });
+    if (!user) {
+        return res.status(401).json({
+            error: "user does not exist"
+        });
     }
 
     const password_correct = user === null
         ? false
         : await bcrypt.compare(body.password, user.password_hash);      //incase of emergrncies start here
 
-    if (!password_correct){
+    if (!password_correct) {
         return res.status(401).json({
-            error : "invalid password"
+            error: "invalid password"
         });
     }
 
     const user_for_token = {
-        username : user.username,
-        id : user._id,
+        username: user.username,
+        id: user._id,
     }
 
     const token = jwt.sign(user_for_token, process.env.SECRET);
 
     res
-    .status(200)
-    .send({token, username: user.username, name: user.name});
+        .status(200)
+        .send({ token, username: user.username, name: user.name });
 });
 
 module.exports = login_router;
