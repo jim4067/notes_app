598dec576baa5acf91a80975db4b38852f04f6ae
jim4067 <jimmyimpulse2@gmail.com>
2020-08-12 16:25:40 +0300

    using morgan to log reqs to the terminal

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const config = require('./utils/config');
 const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 require('express-async-errors');
 const logger = require('./utils/logger')
 const note_router = require('./controllers/notes');
@@ -26,11 +27,13 @@ app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
 
+app.use(morgan('dev'));
+
 app.use('/api/login', login_router);
 app.use('/api/notes', note_router);
 app.use('/api/users', user_router);
 
-app.use(middleware.request_logger)
+//app.use(middleware.request_logger)
 app.use(middleware.unknown_endpoint);
 app.use(middleware.error_handler);
 
