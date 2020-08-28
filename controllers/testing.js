9b8f001de7fb98841b3f13810d281ed20216a46e
jim4067 <jimmyimpulse2@gmail.com>
2020-08-28 13:15:31 +0300

    typo fix: changed the response to send 204 & end once deletion is complete

--- controllers/testing.js
+++ controllers/testing.js
@@ -7,7 +7,7 @@ router.post('/reset', async (req, res) => {
     await Note.deleteMany({});
     await User.deleteMany({});
 
-    res.status(404).end();
+    res.status(204).end();
 });
 
 module.exports = { router }
