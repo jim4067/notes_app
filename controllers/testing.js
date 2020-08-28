4b3d6b51f21f83f6283acbeb7fbced4d50498c4b
jim4067 <jimmyimpulse2@gmail.com>
2020-08-28 14:18:03 +0300

    :beetle: added a HTTP GET request for testing the request

--- controllers/testing.js
+++ controllers/testing.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const Note = require('../models/note');
 const User = require('../models/user');
 
+router.get('/', async (req,res) => {
+    res.send("the route works just fine");
+});
 
 router.post('/reset', async (req, res) => {
     await Note.deleteMany({});
@@ -10,5 +13,5 @@ router.post('/reset', async (req, res) => {
     res.status(204).end();
 });
 
-module.exports = { router }
+module.exports =  router; 
 //route for testing  the databse
